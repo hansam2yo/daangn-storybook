10878cae72191ee3eacede41b358e9f7a15e60cf
hansam2yo <hansam2yo@gmail.com>
2023-05-27 17:01:10 +0900

    mui theme typography, palette 정의

--- src/theme/index.ts
+++ src/theme/index.ts
@@ -1,30 +1,108 @@
 import { createTheme } from "@mui/material";
-import { blueGrey, cyan, pink } from "@mui/material/colors";
+
+const TyphographyOptions = {
+  h1: {
+    fontSize: "34px",
+    lineHeight: "46px",
+    fontStyle: "normal",
+    fontWeight: 700,
+  },
+  h2: {
+    fontSize: "28px",
+    lineHeight: "38px",
+    fontStyle: "normal",
+    fontWeight: 700,
+  },
+  h3: {
+    fontSize: "20px",
+    lineHeight: "30px",
+    fontStyle: "normal",
+    fontWeight: 600,
+  },
+  h4: {
+    fontSize: "18px",
+    lineHeight: "32px",
+    fontStyle: "normal",
+    fontWeight: 700,
+  },
+  h5: {
+    fontSize: "18px",
+    lineHeight: "26px",
+    fontStyle: "normal",
+    fontWeight: 700,
+  },
+  h6: {
+    fontSize: "17px",
+    lineHeight: "25px",
+    fontStyle: "normal",
+    fontWeight: 400,
+  },
+  subtitle1: {
+    fontSize: "16px",
+    lineHeight: "22px",
+    fontWeight: 700,
+    fontStyle: "normal",
+  },
+  subtitle2: {
+    fontSize: "13px",
+    fontStyle: "normal",
+    fontWeight: 400,
+    lineHeight: "20px",
+  },
+  body1: {
+    fontSize: "16px",
+    fontStyle: "normal",
+    lineHeight: "26px",
+    weight: 400,
+  },
+  body2: {
+    fontSize: "14px",
+    lineHeight: "20px",
+    weight: 400,
+    fontStyle: "normal",
+  },
+  caption: {
+    fontSize: "12px",
+    fontWeight: 400,
+    lineHeight: "16px",
+    fontStyle: "normal",
+  },
+  button: {
+    fontStyle: "normal",
+    fontSize: "14px",
+    lineHeight: "20px",
+    fontWeight: 700,
+  },
+  fontFamily:
+    '-apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, "Noto Sans", sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji"',
+};
 
 export const lightTheme = createTheme({
   palette: {
     mode: "light",
     primary: {
-      main: cyan["A200"],
+      main: "#ff7e36",
     },
     secondary: {
-      main: pink["A400"],
+      main: "#f0f3f5",
+    },
+    text: {
+      primary: "#212124",
+      secondary: "#a2a6ad",
     },
   },
+  typography: TyphographyOptions,
 });
 
 export const darkTheme = createTheme({
   palette: {
     mode: "dark",
     primary: {
-      main: pink["A200"],
+      main: "#ff7e36",
     },
     secondary: {
-      main: cyan["A400"],
-    },
-    background: {
-      default: blueGrey["800"],
-      paper: blueGrey["700"],
+      main: "#f0f3f5",
     },
   },
+  typography: TyphographyOptions,
 });
