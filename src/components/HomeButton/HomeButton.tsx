63e9c5bd095df31cdf64b668ab7d9dd77f3e4914
hansam2yo <hansam2yo@gmail.com>
2023-06-05 16:54:19 +0900

    homebutton 수정

--- src/components/HomeButton/HomeButton.tsx
+++ src/components/HomeButton/HomeButton.tsx
@@ -5,13 +5,19 @@ export type HomeButtonProps = {};
 
 const HomeButton: FC<HomeButtonProps> = (props) => {
   return (
-    <Box sx={{ width: "30%" }}>
+    <Box sx={{ width: "50%" }}>
       <Stack spacing={2}>
         <Button
           variant="contained"
           sx={{
-            color: "black",
+            fontSize: "1.8rem",
+            color: "#212529",
             backgroundColor: "#f1f3f5",
+            border: "none",
+            borderRadius: "6px",
+            textDecoration: "none",
+            padding: "1.45rem 2.6rem",
+            display: "inline-block",
           }}
         >
           인기매물 보기
@@ -19,8 +25,14 @@ const HomeButton: FC<HomeButtonProps> = (props) => {
         <Button
           variant="contained"
           sx={{
-            color: "black",
+            fontSize: "1.8rem",
+            color: "#212529",
             backgroundColor: "#f1f3f5",
+            border: "none",
+            borderRadius: "6px",
+            textDecoration: "none",
+            padding: "1.45rem 2.6rem",
+            display: "inline-block",
           }}
         >
           믿을 수 있는 중고거래
