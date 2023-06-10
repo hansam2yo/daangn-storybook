7748b8e09708d155257e72becbc2d62d6b820301
hansam2yo <hansam2yo@gmail.com>
2023-06-10 15:52:45 +0900

    CategoryMenu 리팩토링

--- src/components/CategoryMenu/CategoryMenu.stories.tsx
+++ src/components/CategoryMenu/CategoryMenu.stories.tsx
@@ -1,4 +1,4 @@
-import CategoryMenu from "./CategoryMenu";
+import CategoryMenu, { CategoryMenuProps } from "./CategoryMenu";
 import { Meta, StoryObj } from "@storybook/react";
 
 const meta: Meta<typeof CategoryMenu> = {
@@ -11,6 +11,16 @@ export default meta;
 type Story = StoryObj<typeof CategoryMenu>;
 
 export const Default: Story = {
-  //args로 props제어
-  render: () => <CategoryMenu />,
+  args: {
+    clickable: true,
+    data: [
+      { label: "전체", selected: true },
+      { label: "식당", selected: false },
+      { label: "카페", selected: false },
+      { label: "용달/이사", selected: false },
+      { label: "뷰티/미용", selected: false },
+      { label: "헬스/필라테스/요가", selected: false },
+    ],
+  },
+  render: (args: CategoryMenuProps) => <CategoryMenu {...args} />,
 };
