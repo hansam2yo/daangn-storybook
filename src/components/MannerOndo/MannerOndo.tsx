953ca3f5181a91a5c0ab55fd2a0bcb0420c50fbb
hansam2yo <hansam2yo@gmail.com>
2023-06-20 16:52:48 +0900

    MannerOndo 컴포넌트 구현 및 스토리북 작성

---
+++ src/components/MannerOndo/MannerOndo.tsx
@@ -0,0 +1,44 @@
+import type { FC } from "react";
+import { Box, Slider } from "@mui/material";
+
+const marks = [
+  {
+    value: 0,
+    label: "0°C",
+  },
+  {
+    value: 20,
+    label: "20°C",
+  },
+  {
+    value: 37,
+    label: "37°C",
+  },
+  {
+    value: 100,
+    label: "100°C",
+  },
+];
+
+function valuetext(value: number) {
+  return `${value}°C`;
+}
+
+export type MannerOndoProps = {};
+
+const MannerOndo: FC<MannerOndoProps> = (props) => {
+  return (
+    <Box sx={{ width: 100 }}>
+      <Slider
+        aria-label="Custom marks"
+        defaultValue={20}
+        getAriaValueText={valuetext}
+        step={10}
+        valueLabelDisplay="auto"
+        marks={marks}
+      />
+    </Box>
+  );
+};
+
+export default MannerOndo;
